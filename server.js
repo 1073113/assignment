bb2ca070e4c7faece2c36929757dd24b51a2d56b
David Ho <ocdev8567@gmail.com>
2021-08-16 23:33:53 +0800

    install dependencies

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
-
 import connectDB from "./config/db.js";
 import versionRoutes from "./assignment/assignment.routes.js";
 import valuesRoutes from "./value/value.routes.js";
 import { handleError } from "./filter/httpException.filter.js";
+import xss from "xss-clean";
+import hpp from "hpp";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cors from "cors";
 
-const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
-});
+import path from "path";
+const __dirname = path.resolve();
 
-dotenv.config();
-
+if (process.env.NODE_ENV) {
+  dotenv.config({
+    path: `${__dirname}/.env.${process.env.NODE_ENV}`,
+  });
+} else {
+  dotenv.config();
+}
+console.log(`${__dirname}/.env.${process.env.NODE_ENV}`);
 connectDB();
 
 const app = express();
@@ -25,11 +33,34 @@ app.use(
   })
 );
 
+// Sanatize data
+app.use(mongoSanitize());
+
+// Security header
+app.use(helmet());
+
+// sanitize user input
+app.use(xss());
+
+// rate Limit
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+});
+
 app.use(apiLimiter);
+
+// protect against HTTP Parameter
+app.use(hpp());
+
+// Enable cors
+app.use(cors());
+
+// Routes
 app.use("/api", versionRoutes);
 app.use("/api/values", valuesRoutes);
 
-// app.use(notFound);
+// Http Handler
 app.use(handleError);
 
 const PORT = process.env.PORT || 5000;
